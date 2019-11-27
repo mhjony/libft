9373a1a046010910a359558e94ecdef0bcaff658
mahmudulhasanjhoony <mahmudul.jhony@gmail.com>
2019-11-27 23:02:09 +0200

    strjoin added

---
+++ ft_strjoin.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jony <jony@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/29 17:37:07 by mhasan            #+#    #+#             */
+/*   Updated: 2019/11/04 22:06:24 by jony             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	char *ptr;
+	int i;
+	int len1;
+	int len2;
+	int j;
+
+	len1 = 0;
+	while (s1[len1] != '\0')
+		len1++;
+	len2 = 0;
+	while (s2[len2] != '\0')
+		len2++;
+	ptr = (char *)malloc(sizeof(*ptr) * (len1 + len2 + 1));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+int main(void)
+{
+	char *str1 = "hello";
+	char *str2 = " good morning!";
+
+	printf("%s", ft_strjoin(str1, str2));
+	return (0);
+}
